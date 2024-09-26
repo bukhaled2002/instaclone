309e5a04e5b64fd6f82c16af5153517384fbefa5
bukhaled2002 <Bakhaled.310@gmail.com>
2024-09-26 12:17:03 +0300

    solve cookies error

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,7 @@ app.use(express.urlencoded({ extended: true })); // To parse form data in the re
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://instaclone-backend-xsfg.onrender.com",
-    ],
+    origin: ["http://localhost:5173", "https://instaclone-bxoe.onrender.com"],
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,10 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] },
+  cors: {
+    origin: "https://instaclone-bxoe.onrender.com",
+    methods: ["GET", "POST"],
+  },
 });
 const userSocketMap = {};
 
